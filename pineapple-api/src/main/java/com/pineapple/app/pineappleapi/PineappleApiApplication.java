c44ea2243f9d0add17e288b4590a9b07af01ec61
AylenG <aylen.p.gomez@gmail.com>
2022-03-10 00:00:35 -0300

    Proyecto inicial

---
+++ pineapple-api/src/main/java/com/pineapple/app/pineappleapi/PineappleApiApplication.java
@@ -0,0 +1,13 @@
+package com.pineapple.app.pineappleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PineappleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PineappleApiApplication.class, args);
+	}
+
+}
---
+++ pineapple-api/src/test/java/com/pineapple/app/pineappleapi/PineappleApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pineapple.app.pineappleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PineappleApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
